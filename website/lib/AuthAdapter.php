2ddc4e6376b941a8650ebfaea2de92436f59aec7
Gerhard Kanzler <gerhard.kanzler@gmail.com>
2016-03-04 13:27:07 +0100

    Update

--- website/lib/AuthAdapter.php
+++ website/lib/AuthAdapter.php
@@ -1,9 +1,7 @@
 <?php
-
 namespace Website;
 
 use Pimcore\Model\Object\User;
-use \Website\Tool\Rwo;
 
 class AuthAdapter implements \Zend_Auth_Adapter_Interface {
 
@@ -19,16 +17,17 @@ class AuthAdapter implements \Zend_Auth_Adapter_Interface {
     public function authenticate() {
         $member = User::getByEmail( $this->email, [ 'limit' => 1 ] );
         if($member instanceof User ) {
-
-	        $verifiedPassword = password_verify( $this->password, $member->getPassword() );
-	        var_dump($verifiedPassword);
+            $verifiedPassword = password_verify( $this->password, $member->getPassword() );
+            var_dump($verifiedPassword);
             if($verifiedPassword === true && ($member->getEmail() == $this->email) && $member->isPublished() ) {
-
-                return new \Zend_Auth_Result(\Zend_Auth_Result::SUCCESS, array('id' => $member->getId(), 'email' => $member->getUsername(), 'username' => $member->getUsername() ) );
+                return new \Zend_Auth_Result(\Zend_Auth_Result::SUCCESS, [
+                    'id' => $member->getId(), 
+                    'email' => $member->getUsername(), 
+                    'username' => $member->getUsername()
+                ]);
             } else {
                 return new \Zend_Auth_Result(\Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID, null);
             }
-
         } else {
             return new \Zend_Auth_Result(\Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID, null);
         }
